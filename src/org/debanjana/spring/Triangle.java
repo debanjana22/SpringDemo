3fbfc9fc1b5fdc131532cae016f367ed0bc372b3
Debanjana B <debanjanapersonal@gmail.com>
2020-08-18 23:13:03 +0530

    Autowire byName

--- src/org/debanjana/spring/Triangle.java
+++ src/org/debanjana/spring/Triangle.java
@@ -1,22 +1,31 @@
 package org.debanjana.spring;
 
-import java.util.List;
-
 public class Triangle {
-	private List<Point> points;
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
 	
-	public List<Point> getPoints() {
-		return points;
+	public Point getPointA() {
+		return pointA;
 	}
-	public void setPoints(List<Point> points) {
-		this.points = points;
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
+	}
+	public Point getPointB() {
+		return pointB;
+	}
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+	public Point getPointC() {
+		return pointC;
+	}
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
 	}
 	
 	public void draw() {
-		for (Point point : points) {
-			System.out.println("Point: (" + point.getX() +"," + point.getY() + ")");
-		}
-		//System.out.println("Point A: (" + getPointA().getX() + "," + getPointA().getY() + "), Point B: (" + getPointB().getX() + "," + getPointB().getY() + "), Point C: (" + getPointC().getX() + "," + getPointC().getY() + ")");
+		System.out.println("Point A: (" + getPointA().getX() + "," + getPointA().getY() + "), Point B: (" + getPointB().getX() + "," + getPointB().getY() + "), Point C: (" + getPointC().getX() + "," + getPointC().getY() + ")");
 	}
 }
 
